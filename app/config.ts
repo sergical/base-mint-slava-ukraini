825266bc34738b6487ed08c2301509ab80d207c6
Sergiy Dybskiy <s@serg.tech>
2024-02-24 14:01:42 -0700

    base address

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
-export const NEXT_PUBLIC_URL = 'https://slava-ukraini.vercel.app';
-export const ALLOWED_ORIGIN = 'slava-ukraini.vercel.app';
-export const ZORA_COLLECTION_ADDRESS = '0xedc77a809e772f549df24a7188982af3be1eafd7';
+export const NEXT_PUBLIC_URL = 'https://slava-ukraini-mint.vercel.app';
+export const ALLOWED_ORIGIN = 'slava-ukraini-mint.vercel.app';
+export const ZORA_COLLECTION_ADDRESS = '0x006b342492a386bFd0D2947f2A2e1010fc5E3215';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
   width: 800,
--- contracts/zora-minter/script/Deploy.s.sol
+++ contracts/zora-minter/script/Deploy.s.sol
@@ -7,7 +7,7 @@ import {Script, console} from "forge-std/Script.sol";
 contract Deploy is Script {
     address internal referrer = address(0x86924c37A93734E8611Eb081238928a9d18a63c0);
     address internal signer = address(0x44DD9B89d4087246A0Fc54dba0c69000a4F59162);
-    address internal collection = address(0xF5a3b6DEE033AE5025E4332695931CaDeB7f4D2B);
+    address internal collection = address(0x006b342492a386bfd0d2947f2a2e1010fc5e3215);
     address internal minter = address(0xFF8B0f870ff56870Dc5aBd6cB3E6E89c8ba2e062);
 
     function run() public {
